ae33bfe8f0eb03d8576a5ce1ae2265e6fd75ae35
VISHWA RAJ SHAH <vishwarajshah@gmail.com>
2022-05-11 17:34:12 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Cards from './components/Cards'
+import Timer from './components/Timer';
+function App() {
+  return (
+    <div className="App">
+      <h1>Memory Game</h1>
+      <Timer/>
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
